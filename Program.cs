a2cf3632484a297fcab25cedd097c1be1489211f
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2022-10-16 02:08:48 +0600

    changes in homepage

--- Program.cs
+++ Program.cs
@@ -1,20 +1,31 @@
+using introapp.Data;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swaggerGenOptions =>
+{
+    swaggerGenOptions.SwaggerDoc("v1", new OpenApiInfo{ Title = "ASP.NET and React tutorial", Version = "v1"});
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+
+app.UseSwaggerUI(swaggerUIOptions =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    swaggerUIOptions.DocumentTitle = "ASP.NET & React intro";
+    swaggerUIOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "Web api service");
+    swaggerUIOptions.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
+app.MapGet("/get-all-posts", async() => PostsRepository.GetPostsAsync())
+    .WithTags("Posts Endpoints");
 
 app.Run();
